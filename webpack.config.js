aea6f2485aa949d1e3ebfae16c377194341a2732
Tiger Wang <tigerwang@outlook.com>
2022-11-28 01:27:54 +0000

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
-const path = require("path");
+const path = require('path');
+
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-            },
-        ]
-    },
-    devServer: {
-        static: "./dist"
-    }
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  devServer: {
+    static: './public',
+  },
+  optimization: {
+    minimize: false,
+  },
 };
